767ee9ea2203fc1d5783456abedeaf9792cc70be
Sercan Parker <sercan.sensulun@outlook.com>
2018-11-15 20:50:03 +0300

    master coder commit.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!haha.");
+		System.out.println("Hello World!haha.I am master.");
 		System.out.println("This was creted on Branch - 1.");
 		
 	}
